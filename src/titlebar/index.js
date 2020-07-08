fe3582580f603b0cc3b9cf0d6bacfc4cde01f88f
Cliemtor Fabros <cliemtor08@gmail.com>
2020-07-08 18:07:41 +0800

    Update component style

--- src/titlebar/index.js
+++ src/titlebar/index.js
@@ -1,14 +1,15 @@
 import './styles.css'
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Container } from '../../src'
 
 function Titlebar(props) {
   const styleName = [props.component, props.className].join(' ').trim()
   return (
-    <p className={styleName}>
+    <Container className={styleName}>
       <span className="titlebar-abbr">{props.abbr}</span>
       <span className="titlebar-title">{props.children}</span>
-    </p>
+    </Container>
   )
 }
 
