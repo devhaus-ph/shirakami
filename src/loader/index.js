1aff99477326683cceb8782e529c37ff80f5346e
Cliemtor Fabros <cliemtor08@gmail.com>
2020-11-11 16:54:27 +0800

    Add loader component

---
+++ src/loader/index.js
@@ -0,0 +1,8 @@
+import './styles.css'
+import React from 'react'
+
+function Loader() {
+  return <svg className="sk-loader" />
+}
+
+export default Loader
---
+++ src/loader/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Loader } from '../'
+
+export default {
+  title: 'Elements/Loader',
+  component: Loader,
+}
+
+export const basic = () => <Loader />
